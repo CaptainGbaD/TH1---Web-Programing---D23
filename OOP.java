d0834ee98ee58eb36b0bdb697b5fd16c88e96902
CaptainGbaD <hle14838@gmail.com>
2024-10-02 09:33:43 +0700

    first commit

---
+++ OOP.java
@@ -0,0 +1,9 @@
+import test.test;
+
+public class OOP {
+    public static void main(String[] args) {
+        test x = new test();
+        x.setI(10);
+        System.out.println(x.getI());
+    }
+}
---
+++ test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test {
+    private int i;
+    public int getI() {
+        return i;
+    }
+    public void setI(int i) {
+        this.i = i;
+    }
+}
